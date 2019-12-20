4d5aaaecffaec6a10f48959ccba261b25757026a
sachin <sraturi@sfu.ca>
2019-12-19 23:34:13 -0800

    added uploading files and updated readme instructions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 flask training
+
+install pip3 and flask
+
+Ubuntu
+   pip3 install pipenv
+   pipenv install 
+   pipenv install flask
+
+To run the application
+ pipenv shell
+ flask run
--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for,flash
 import json
 import os.path
-
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.secret_key = 'gfchf45rgcgvhb67vhbnj6ftftyt5yyg'
 
@@ -28,7 +28,14 @@ def your_url():
             flash('oops looks like the short name has already been taken!')
             return redirect(url_for('home'))
 
-        urls[request.form['code']] = {'url': request.form['url']}
+        if 'url'in request.form.keys():
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = request.form['code']+ secure_filename(f.filename)
+            f.save('/home/sachin/Desktop/PROJECTS/flask training/url-shortner/files/'+full_name)
+            urls[request.form['code']] = {'file':full_name}
+        
         with open('urls.json','w') as url_file:
             json.dump(urls,url_file)
         return render_template('your_url.html',code = request.form['code'])

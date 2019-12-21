e98e815647d1634c5c5a5c72079b03b0c2bcaa16
sachin <sraturi@sfu.ca>
2019-12-20 22:44:38 -0800

    implemented blueprint

--- urlshort/urlshort.py
+++ urlshort/urlshort.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for,flash, abort, session, jsonify
+from flask import Flask, render_template, request, redirect, url_for,flash, abort, session, jsonify,Blueprint
 import json
 import os.path
 from werkzeug.utils import secure_filename
-app = Flask(__name__)
-app.secret_key = 'gfchf45rgcgvhb67vhbnj6ftftyt5yyg'
 
-@app.route('/')
+bp = Blueprint('urlshort',__name__)
+
+@bp.route('/')
 def home():
     return render_template('home.html', codes = session.keys())
 
 
-@app.route('/your-url', methods=['GET','POST'])
+@bp.route('/your-url', methods=['GET','POST'])
 def your_url():
     if request.method == 'POST':
         urls = {}
@@ -26,14 +26,14 @@ def your_url():
         # code and url are the variable we get from home.html file
         if request.form['code'] in urls.keys():
             flash('oops looks like the short name has already been taken!')
-            return redirect(url_for('home'))
+            return redirect(url_for('urlshort.home'))
 
         if 'url'in request.form.keys():
             urls[request.form['code']] = {'url': request.form['url']}
         else:
             f = request.files['file']
             full_name = request.form['code']+ secure_filename(f.filename)
-            f.save('/home/sachin/Desktop/PROJECTS/flask training/url-shortner/static/files/'+full_name)
+            f.save('/home/sachin/Desktop/PROJECTS/flask training/url-shortner/urlshort/static/files/'+full_name)
             urls[request.form['code']] = {'file':full_name}
         
         with open('urls.json','w') as url_file:
@@ -43,13 +43,13 @@ def your_url():
     else:
         #if there is someone is trying to do a get request, send them bk to home
         #url_for makes page based on function name
-        return redirect(url_for('home'))
+        return redirect(url_for('urlshort.home'))
 
-@app.route('/about')
+@bp.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/<string:code>')
+@bp.route('/<string:code>')
 def redirect_to_url(code):
     if os.path.exists('urls.json'):
         with open('urls.json') as url_file:
@@ -63,11 +63,11 @@ def redirect_to_url(code):
     return abort(404)
 
 
-@app.errorhandler(404)
+@bp.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'),404
 
 
-@app.route('/api')
+@bp.route('/api')
 def session_api():
     return jsonify(list(session.keys()))
